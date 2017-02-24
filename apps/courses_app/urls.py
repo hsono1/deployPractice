019c03e42586e5f4ce21cd3e139821a5d0563abb
Hans Sono <hans.sono23@gmail.com>
2017-02-23 19:12:44 -0800

    new app

--- apps/courses_app/urls.py
+++ apps/courses_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^validate$', views.validate),
     url(r'^success$', views.success),
     url(r'^remove/(?P<id>\d+)$', views.remove),
+
 ]
--- courses/urls.py
+++ courses/urls.py
@@ -1,4 +1,4 @@
-"""emailValidation URL Configuration
+"""courses URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -16,5 +16,5 @@ Including another URLconf
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^', include("apps.email_val.urls")),
+    url(r'^', include('apps.courses_app.urls')),
 ]
